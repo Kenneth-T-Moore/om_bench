c33c6bef5fbd07830b04f90cb6a0c7b94d2903b7
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2018-02-15 17:28:58 -0500

    added beam

---
+++ scripts/beam.py
@@ -0,0 +1,40 @@
+"""
+Generate benchmarking data for the Beam problem.
+"""
+from __future__ import print_function
+
+import numpy as np
+
+from om_bench.bench import Bench
+
+from openmdao.test_suite.test_examples.beam_optimization.beam_group import BeamGroup
+
+
+class BeamBench(Bench):
+
+    def setup(self, problem, ndv, nstate, nproc):
+        E = 1.
+        L = 1.
+        b = 0.1
+        volume = 0.01
+
+        num_elements = 50 * nstate
+
+        problem.model = BeamGroup(E=E, L=L, b=b, volume=volume, num_elements=num_elements)
+
+
+desvars = [1]
+states = [1, 5, 10]
+procs = [1]
+
+bench = BeamBench(desvars, states, procs)
+
+bench.run_benchmark()
+
+
+
+
+
+
+
+
