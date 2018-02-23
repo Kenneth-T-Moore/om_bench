2363c8e566074a2288046b29a264cd873d47e28d
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2018-02-23 11:15:54 -0500

    ready to try on cluster

--- om_bench/bench.py
+++ om_bench/bench.py
@@ -183,7 +183,7 @@ class Bench(object):
                         self._prepare_pbs_job(nproc, nstate, ndv, j, name)
 
                         # Submit job
-                        # p = subprocess.Popen(["qsub", '%s.py' % name])
+                        p = subprocess.Popen(["qsub", '%s.py' % name])
 
         print("All jobs submitted.")
 
