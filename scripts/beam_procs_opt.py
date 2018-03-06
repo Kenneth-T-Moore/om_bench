cea5a60136d9a06fbc5f01570786c053408f12b1
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2018-03-06 16:19:30 -0500

    beam opt procs

---
+++ scripts/beam_procs_opt.py
@@ -0,0 +1,55 @@
+"""
+Generate benchmarking data for the Beam problem.
+"""
+from __future__ import print_function
+
+import numpy as np
+
+from om_bench.bench import Bench
+
+from openmdao.api import ScipyOptimizeDriver, pyOptSparseDriver
+from openmdao.test_suite.test_examples.beam_optimization.multipoint_beam_group import MultipointBeamGroup
+
+
+class BeamBench(Bench):
+
+    def setup(self, problem, ndv, nstate, nproc):
+        E = 1.
+        L = 1.
+        b = 0.1
+        volume = 0.01
+        num_elements = 50 * nstate
+        num_cp = ndv * 40
+        num_load_cases = 32
+
+        problem.model = MultipointBeamGroup(E=E, L=L, b=b, volume=volume, num_elements=num_elements,
+                                            num_cp=num_cp, num_load_cases=num_load_cases)
+
+        problem.driver = pyOptSparseDriver()
+        problem.driver.options['optimizer'] = 'SNOPT'
+        #problem.driver.options['tol'] = 1e-9
+        #problem.driver.options['disp'] = True
+
+    def post_setup(self, problem, ndv, nstate, nproc):
+        pass
+
+    def post_run(problem):
+        # Check stuff here.
+        pass
+
+
+if __name__ == "__main__":
+
+    desvars = [1]
+    states = [1]
+    procs = [1, 2, 4, 8, 16, 32]
+
+    bench = BeamBench(desvars, states, procs, name='beam')
+    bench.num_averages = 1
+    bench.walltime = 1
+    bench.time_nonlinear = False
+    bench.time_linear = False
+    bench.time_driver = True
+    bench.single_batch = True
+
+    bench.run_benchmark_mpi()
